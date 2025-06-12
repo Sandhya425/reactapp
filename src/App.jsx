096e664df422d6b8676de55064542c03b272e584
sandhya425 <rajputsandhya425@gmail.com>
2025-06-12 16:58:41 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
+// const App=()=>{
+//   return(
+//     <>
+//     hello
+//     <h1>ee</h1>
+//     <h1>ee</h1>
+//     <h1>ee</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+// const App=()=>{
+
+//   const s1={
+//     color:"yellow",
+//     backgroundColor:"black"
+//   }
+//   const s2={color:"yellow", backgroundColor:"black"}
+//   return(
+//     <>
+//     hello
+//     <h1 style={s1}>ee</h1>
+//     <h1 style={s2}>ee</h1>
+//     <h1 style={{color:"yellow", backgroundColor:"black"}}>ee</h1>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+import "./style.css"
 const App=()=>{
+
+  const s1={
+    color:"yellow",
+    backgroundColor:"black"
+  }
+  const s2={color:"yellow", backgroundColor:"black"}
   return(
     <>
     hello
-    <h1>ee</h1>
-    <h1>ee</h1>
-    <h1>ee</h1>
+    <h1 style={s1}>ee</h1>
+    <h1 style={s2}>ee</h1>
+    <h1 style={{color:"yellow", backgroundColor:"black"}}>ee</h1>
+    <div className="div1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, sit.</div>
     </>
   )
 }
